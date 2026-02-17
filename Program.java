729a468ec2bf54635fb20dfc16e99ff035385aeb
rifaqatahamed <agrahamed07@gmail.com>
2026-02-17 11:11:49 +0530

    OOPSBannerAPP

--- Program.java
+++ Program.java
@@ -1,5 +1,6 @@
 public class Program {
     public static void main(String[] args) {
+        System.out.println("OOPS BANNER APP: \n");
         System.out.println("   ***    " + "   ***    " + "******    " + "   ***** ");
 		System.out.println(" **   **  " + " **   **  " + "**    **  " + " **      ");
 		System.out.println("**     ** " + "**     ** " + "**     ** " + "**       ");

e092c105894ce99d682d95bc2bfecbe7e9a1be4f
rifaqatahamed <agrahamed07@gmail.com>
2026-02-23 08:28:46 +0530

    Initial commit

---
+++ OODPBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * Printing OOPS
+ * @author RIFAQAT AHAMED A G
+ * @version 1.1
+ */
+
+public class OODPBannerApp{
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "*****", " ","*****", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*    ", " ", "*****"));                
+    }
+}
